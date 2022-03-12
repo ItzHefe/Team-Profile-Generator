e6a55241f9b5c135b7e8ac3dd9adfd695bcd27e2
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-03-12 09:20:27 -0500

    compartmentalizing employee classes and roles to push back to the main app

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 let teamArr = [];
 
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 function askManager() {
   return inquirer.prompt([
     {
@@ -30,6 +34,7 @@ function askManager() {
   // call the mainMenu() function
   .then(answers => { 
     teamArr.push(answers)
+    console.log(teamArr)
     mainMenu()
   })
  
@@ -85,6 +90,7 @@ function engineerQ() {
   ])
   .then(answers => {
     teamArr.push(answers)
+    console.log(teamArr)
     mainMenu()
   })
 }
@@ -119,38 +125,71 @@ function internQ() {
   ])
   .then(answers => {
     teamArr.push(answers)
+    console.log(teamArr)
     mainMenu()
   })
 }
 
+
 function buildTeam() {
-  fs.writeFileSync('index.html', generateHTML(teamArr))
+fs.writeFileSync('index.html', generateHTML(teamArr))
+console.log(teamArr)
+teamArr.map(console.log(teamArr.name))
 };
 
 
 const generateHTML = (teamArr) => //build out the HTML and use .map to loop through the teamArr and drop the members on the page in a card. map function to append new cards per person input added.
   `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  ${teamArr.map()}
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">My Team</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+          integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  
+      <title>Document</title>
+  </head>
+  
+  <body>
+  ${teamArr.map(person => {
+    return ``
+  })}
+      <nav class="navbar navbar-dark bg-primary justify-content-center">
+          <span class="navbar-text text-white font-weight-bolder">
+              My Team
+          </span>
+      </nav>
+  
+      <ul class="list-group container">
+          <div class="row">
+              <div class="card text-white bg-primary col-sm-2 col-md-3 col-lg-4 ml-2 mb-3 p-2 mt-2 rounded"
+                  style="max-width: 18rem;" id="empCards">
+                  <div class="card-header" id="personCard">
+                      <h5 class="card-title"></i>Example Name</h5>
+                  </div>
+                  <div class="card-body" id="result-content">
+                      <ul class="list-group list-group-flush">
+                          <li class="list-group-item text-dark">A item</li>
+                          <li class="list-group-item text-dark">A second item</li>
+                          <li class="list-group-item text-dark">A third item</li>
+                      </ul>
+                  </div>
+              </div>
+      </ul>
+  
+  
+  </body>
+  
+  </html>`;
 
 askManager();
+
+// peopleArr.map(person => {
+//   return `<h1>${person.name}</h1>
+// <h3>${person.age}</h3>`;
+// })
+
+//${teamArr.map()}
+
+//break apart the HTML to create the cards with their own functions and appending them to a bootstrap container to keep it neat looking
